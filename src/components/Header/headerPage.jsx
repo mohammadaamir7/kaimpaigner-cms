15e28668fbb00d7631dec0fa671d03ffe6154c98
Mohammad Aamir <arplayer26@gmail.com>
2021-03-10 16:53:24 -0800

    CMS heroku

--- src/components/Header/headerPage.jsx
+++ src/components/Header/headerPage.jsx
@@ -10,9 +10,9 @@ import grid from '../../assets/img/grid.png'
 import volume from '../../assets/img/volume.png'
 import Vector from '../../assets/img/Vector.png'
 import time from '../../assets/img/Time Square.png'
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+// import * as am4core from "@amcharts/amcharts4/core";
+// import * as am4charts from "@amcharts/amcharts4/charts";
+// import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 export default class HeaderPage extends Component{
     constructor(props){
@@ -278,39 +278,6 @@ export default class HeaderPage extends Component{
         //       chart.scrollbarX = new am4core.Scrollbar();
 
 
-
-        let chart = am4core.create("chartdiv", am4charts.XYChart);
-
-    chart.paddingRight = 20;
-
-    let data = [];
-    let visits = 10;
-    for (let i = 1; i < 366; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-    }
-
-    chart.data = data;
-
-    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
-
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    valueAxis.renderer.minWidth = 35;
-
-    let series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
-
-    series.tooltipText = "{valueY.value}";
-    chart.cursor = new am4charts.XYCursor();
-
-    let scrollbarX = new am4charts.XYChartScrollbar();
-    scrollbarX.series.push(series);
-    chart.scrollbarX = scrollbarX;
-
-    this.chart = chart;
 
  
     }
@@ -3554,7 +3521,7 @@ export default class HeaderPage extends Component{
 
                             <br/><br/>
 
-                            <div id='chartdiv' style={{width: '90%', height: '500px'}}></div>
+                            {/* <div id='chartdiv' style={{width: '90%', height: '500px'}}></div> */}
 
                             <br/><br/><br/>
                             
