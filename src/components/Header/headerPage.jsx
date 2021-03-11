9e55cc3ee322350e96ece803f22d19d30f312b35
Mohammad Aamir <arplayer26@gmail.com>
2021-03-11 15:12:34 -0800

    CMS heroku

--- src/components/Header/headerPage.jsx
+++ src/components/Header/headerPage.jsx
@@ -14,6 +14,8 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
+
+am4core.useTheme(am4themes_animated);
 export default class HeaderPage extends Component{
     constructor(props){
         super(props)
